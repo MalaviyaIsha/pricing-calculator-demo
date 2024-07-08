d26f0be34df0081eb87bb0af9f4664c3452ff1e3
MalaviyaIsha <isha.malaviya@exhibytesolution.com>
2024-07-08 14:22:05 +0530

    change node version

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var $6gDYh$reactjsxruntime = require("react/jsx-runtime");
-var $6gDYh$reactdomclient = require("react-dom/client");
-var $6gDYh$react = require("react");
+var $j4LXS$reactjsxruntime = require("react/jsx-runtime");
+var $j4LXS$reactdomclient = require("react-dom/client");
+var $j4LXS$react = require("react");
 
 
 function $parcel$interopDefault(a) {
@@ -11,42 +11,40 @@ function $parcel$interopDefault(a) {
 
 
 
-var $80a86920d04502a3$var$About = function() {
-    return /*#__PURE__*/ (0, ($parcel$interopDefault($6gDYh$react))).createElement("div", null, /*#__PURE__*/ (0, ($parcel$interopDefault($6gDYh$react))).createElement("section", {
+var $92c4f1c6e9b6326f$var$About = function() {
+    return /*#__PURE__*/ (0, ($parcel$interopDefault($j4LXS$react))).createElement("div", null, /*#__PURE__*/ (0, ($parcel$interopDefault($j4LXS$react))).createElement("section", {
         className: "text-gray-600 body-font"
-    }, /*#__PURE__*/ (0, ($parcel$interopDefault($6gDYh$react))).createElement("div", {
+    }, /*#__PURE__*/ (0, ($parcel$interopDefault($j4LXS$react))).createElement("div", {
         className: "container mx-auto flex px-5 py-24 items-center justify-center flex-col"
-    }, /*#__PURE__*/ (0, ($parcel$interopDefault($6gDYh$react))).createElement("div", {
+    }, /*#__PURE__*/ (0, ($parcel$interopDefault($j4LXS$react))).createElement("div", {
         className: "text-center lg:w-2/3 w-full"
-    }, /*#__PURE__*/ (0, ($parcel$interopDefault($6gDYh$react))).createElement("h1", {
+    }, /*#__PURE__*/ (0, ($parcel$interopDefault($j4LXS$react))).createElement("h1", {
         className: "title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900"
-    }, "Tailwind CSS Design Example"), /*#__PURE__*/ (0, ($parcel$interopDefault($6gDYh$react))).createElement("p", {
+    }, "Tailwind CSS Design Example"), /*#__PURE__*/ (0, ($parcel$interopDefault($j4LXS$react))).createElement("p", {
         className: "mb-8 leading-relaxed"
-    }, "This is a simple design example using Tailwind CSS. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."), /*#__PURE__*/ (0, ($parcel$interopDefault($6gDYh$react))).createElement("div", {
+    }, "This is a simple design example using Tailwind CSS. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."), /*#__PURE__*/ (0, ($parcel$interopDefault($j4LXS$react))).createElement("div", {
         className: "flex justify-center"
-    }, /*#__PURE__*/ (0, ($parcel$interopDefault($6gDYh$react))).createElement("button", {
+    }, /*#__PURE__*/ (0, ($parcel$interopDefault($j4LXS$react))).createElement("button", {
         className: "inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
     }, "Button"))))));
 };
-var $80a86920d04502a3$export$2e2bcd8739ae039 = $80a86920d04502a3$var$About;
+var $92c4f1c6e9b6326f$export$2e2bcd8739ae039 = $92c4f1c6e9b6326f$var$About;
 
 
-var $cff1ea2022d900b0$var$Header = function() {
-    return /*#__PURE__*/ (0, ($parcel$interopDefault($6gDYh$react))).createElement("div", null, /*#__PURE__*/ (0, ($parcel$interopDefault($6gDYh$react))).createElement((0, $80a86920d04502a3$export$2e2bcd8739ae039), null));
+var $30a98339f70aa6ab$var$Header = function() {
+    return /*#__PURE__*/ (0, ($parcel$interopDefault($j4LXS$react))).createElement("div", null, /*#__PURE__*/ (0, ($parcel$interopDefault($j4LXS$react))).createElement((0, $92c4f1c6e9b6326f$export$2e2bcd8739ae039), null));
 };
-var $cff1ea2022d900b0$export$2e2bcd8739ae039 = $cff1ea2022d900b0$var$Header;
+var $30a98339f70aa6ab$export$2e2bcd8739ae039 = $30a98339f70aa6ab$var$Header;
 
 
-var $0235e0f21c71529f$var$App = function() {
-    return /*#__PURE__*/ (0, ($parcel$interopDefault($6gDYh$react))).createElement("div", null, /*#__PURE__*/ (0, ($parcel$interopDefault($6gDYh$react))).createElement("h1", {
-        className: "text-[green]"
-    }, "Hello"), /*#__PURE__*/ (0, ($parcel$interopDefault($6gDYh$react))).createElement((0, $cff1ea2022d900b0$export$2e2bcd8739ae039), null));
+var $2056b1197bd855ab$var$App = function() {
+    return /*#__PURE__*/ (0, ($parcel$interopDefault($j4LXS$react))).createElement("div", null, /*#__PURE__*/ (0, ($parcel$interopDefault($j4LXS$react))).createElement((0, $30a98339f70aa6ab$export$2e2bcd8739ae039), null));
 };
-var $0235e0f21c71529f$export$2e2bcd8739ae039 = $0235e0f21c71529f$var$App;
+var $2056b1197bd855ab$export$2e2bcd8739ae039 = $2056b1197bd855ab$var$App;
 
 
-const $d6ba8da31e566df5$var$container = document.getElementById("app");
-const $d6ba8da31e566df5$var$root = (0, $6gDYh$reactdomclient.createRoot)($d6ba8da31e566df5$var$container);
-$d6ba8da31e566df5$var$root.render(/*#__PURE__*/ (0, $6gDYh$reactjsxruntime.jsx)((0, $0235e0f21c71529f$export$2e2bcd8739ae039), {}));
+const $8fa96f18ea0e3983$var$container = document.getElementById("app");
+const $8fa96f18ea0e3983$var$root = (0, $j4LXS$reactdomclient.createRoot)($8fa96f18ea0e3983$var$container);
+$8fa96f18ea0e3983$var$root.render(/*#__PURE__*/ (0, $j4LXS$reactjsxruntime.jsx)((0, $2056b1197bd855ab$export$2e2bcd8739ae039), {}));
 
 
